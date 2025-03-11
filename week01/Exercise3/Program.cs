7c5667fa34e8d0fcab577c4b7f7c96461b368709
ncard05 <ncardenas@byupathway.edu>
2025-03-11 12:17:35 -0500

    Completed Exersize3 with 1 stretch challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        bool correct = false;
+        int guesses = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string sGuess = Console.ReadLine();
+            int guess = int.Parse(sGuess);
+
+            if (guess == number)
+            {
+                correct = true;
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            guesses = guesses + 1;
+
+        }while (correct == false);
+
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"That took you {guesses} guesses!");
+
     }
 }
